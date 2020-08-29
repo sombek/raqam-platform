00c79c4f536cded77dfcf7bafe6847adc3f111fe
abdullah <sombek990@hotmail.com>
2020-08-29 14:00:11 +0300

    update Services.vue text positions

--- src/views/components/Services.vue
+++ src/views/components/Services.vue
@@ -3,33 +3,35 @@
 		<img src="../../assets/computer.gif" height="200" alt="">
 		<h2>ماذا نقدم؟</h2>
 
-		<p style="text-align: right">
-			تنقسم خدماتنا بشكل أساسي إلى قسمين: الخدمات الإستشارية و الخدمات التطويرية.
-		</p>
-		<p style="text-align: right">
-			و عليها يتم العمل لتحديد المتطلبات الخاصة بالعميل ويتم تضمينها مثل الخدمات ادناه:
-		</p>
-		<ul>
-			<li>
-				تقديم الاستشارات المتعلقة بالمعمارية التقنية القائمة
-			</li>
-			<li>
-				تشخيص و توضيح المخاطر التقنية للحلول التقنية القائمة
-			</li>
-			<li>
-				تحليل و تطوير إجراءات الأعمال و وضع حلول رقمية
-			</li>
-			<li>
-				تطوير، و تصميم، و تنفيذ الحلول التقنية الجديدة
-			</li>
-			<li>
-				إستخدام التقنيات المتطورة للعمليات التقنية. مثل: الـ
-				<span style="font-size:18pt;font-family: 'Pixelar Regular W01 Regular', serif;">
+		<div class="text">
+
+			<p style="text-align: right">
+				تنقسم خدماتنا بشكل أساسي إلى قسمين: الخدمات الإستشارية و الخدمات التطويرية.
+			</p>
+			<p style="text-align: right">
+				و عليها يتم العمل لتحديد المتطلبات الخاصة بالعميل ويتم تضمينها مثل الخدمات ادناه:
+			</p>
+			<ul>
+				<li>
+					تقديم الاستشارات المتعلقة بالمعمارية التقنية القائمة
+				</li>
+				<li>
+					تشخيص و توضيح المخاطر التقنية للحلول التقنية القائمة
+				</li>
+				<li>
+					تحليل و تطوير إجراءات الأعمال و وضع حلول رقمية
+				</li>
+				<li>
+					تطوير، و تصميم، و تنفيذ الحلول التقنية الجديدة
+				</li>
+				<li>
+					إستخدام التقنيات المتطورة للعمليات التقنية. مثل: الـ
+					<span style="font-size:18pt;font-family: 'Pixelar Regular W01 Regular', serif;">
 					DevOps
 				</span>
-			</li>
-
-		</ul>
+				</li>
+			</ul>
+		</div>
 
 	</div>
 </template>
@@ -39,27 +41,31 @@
 	}
 </script>
 <style scoped>
-	h1 {
-		margin: unset;
-		color: white;
-		font-size: 92px;
-		font-weight: 100;
+	.services {
+		width: 60%;
+		color: #eaecce;
+		margin: auto auto 50px;
 	}
 
-	h3 {
-		margin: unset;
-		color: white;
+	h2 {
+		font-size: 32pt;
 		font-weight: 100;
+		margin-top: 0;
 	}
 
 	@media only screen and (max-width: 768px) {
-		h1 {
-			font-size: 12vw;
-		}
-
 		div.services {
 			width: 90%;
 		}
+
+		div.text {
+			width: 100%;
+		}
+	}
+
+	.text {
+		width: 70%;
+		margin: auto;
 	}
 
 	li:before {
@@ -70,19 +76,5 @@
 		text-align: right;
 		list-style: none;
 	}
-
-	.services {
-		width: 60%;
-		color: #eaecce;
-		margin: auto auto 50px;
-
-	}
-
-	.services h2 {
-		font-size: 32pt;
-		font-weight: 100;
-		margin-top: 0;
-	}
-
 
 </style>
